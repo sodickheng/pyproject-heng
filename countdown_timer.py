f577f1d8fd32f2d8fc373b5f25f173a37c4dcc7b
Heng <hengsodick1@gmail.com>
2024-07-17 23:19:52 +0800

    Add files via upload

--- countdown_timer.py
+++ countdown_timer.py
@@ -1,18 +1,20 @@
+# import the module
 import time
 
+# define the countdown func.
 def countdown(t):
-
+#This line starts a while loop that will continue to run as long as t is not zero.
     while t:
         mins, secs = divmod(t, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer)
-        print(timer, end="\r")
+        print(timer, end="\r")  # use terminal to see timer overwrite
         time.sleep(1)
         t -=1
 
+    print("Countdown timer Ended")
 
-    print("Countdown timer End")
+# input time in seconds
+t = input("Enter the time in seconds: ")
 
-t = input("Enter the time in seconds ")
-
+# function call
 countdown(int(t))
