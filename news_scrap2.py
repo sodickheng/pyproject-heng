288704371376e05090048b36f12a87382e086a50
Heng <hengsodick1@gmail.com>
2024-08-12 09:55:39 +0800

    Update news_scrap2.py

--- news_scrap2.py
+++ news_scrap2.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.amazon.sg/Owala-FreeSip-Insulated-Stainless-BPA-Free/dp/B0C59CLN29?pd_rd_w=IlzG5&content-id=amzn1.sym.11079857-0102-48e6-9566-cd773a0fdaf4&pf_rd_p=11079857-0102-48e6-9566-cd773a0fdaf4&pf_rd_r=F1TTE6J6FBMK22VGH20E&pd_rd_wg=ZMACt&pd_rd_r=5b709d18-cbbc-4cdc-8106-e10a932b59d5&pd_rd_i=B0C59CLN29&ref_=oct_dx_dotd_B0C59CLN29&th=1"
+url = ""
 
 headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"}
 page = requests.get(url, headers=headers)
